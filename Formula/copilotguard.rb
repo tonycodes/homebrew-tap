66e8545e578b9d156596c622457bae96262a6d62
Tony James <me@tony.codes>
2026-01-19 10:20:00 +0000

    Update copilotguard to 0.1.0 with real checksums

--- Formula/copilotguard.rb
+++ Formula/copilotguard.rb
@@ -7,18 +7,18 @@ class Copilotguard < Formula
   on_macos do
     on_arm do
       url "https://github.com/tonycodes/copilotguard-daemon/releases/download/v#{version}/copilotguard-darwin-arm64.tar.gz"
-      sha256 "PLACEHOLDER_DARWIN_ARM64"
+      sha256 "78fea969a3084d9b3e9c6526b1e27a16a31e7c4eaf8237060400b88fcddb7ba4"
     end
     on_intel do
       url "https://github.com/tonycodes/copilotguard-daemon/releases/download/v#{version}/copilotguard-darwin-amd64.tar.gz"
-      sha256 "PLACEHOLDER_DARWIN_AMD64"
+      sha256 "2f962481eb2b1c5df61c0c108d97c0958005aa0198f6056af7b1c286ea61ec7b"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/tonycodes/copilotguard-daemon/releases/download/v#{version}/copilotguard-linux-amd64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_AMD64"
+      sha256 "a384c4c2fe0ea181cc79c9da587460c0531dcc41a0559e3fb416da0ced9796c9"
     end
   end
 
